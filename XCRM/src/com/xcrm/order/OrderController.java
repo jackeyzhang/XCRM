5a96b9a40d5e6324b0aebf28ec672b486b2197ff
venlv2046 <venlv2046@gmail.com>
2016-12-18 10:48:45 +0800

    test

--- XCRM/src/com/xcrm/order/OrderController.java
+++ XCRM/src/com/xcrm/order/OrderController.java
@@ -1,46 +1,30 @@
 package com.xcrm.order;
 
+import java.util.Iterator;
+import java.util.List;
+
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Page;
+import com.jfinal.plugin.activerecord.Record;
 import com.xcrm.common.AbstractController;
+import com.xcrm.common.AttributeFinder;
+import com.xcrm.common.Pager;
+import com.xcrm.common.model.Attribute;
+import com.xcrm.common.model.Attributevalue;
 import com.xcrm.common.util.Constant;
 
 public class OrderController extends AbstractController {
-	
-	
-	public void list(){/*
-		String sql = "select oi.id id,p.name name,o.orderno orderno,bi.price price,bi.num num,GROUP_CONCAT(pic.fielname) filename from orderitem oi left join bookitem bi on oi.bookitem=bi.id left join `order` o on o.id=oi.order left join product p on bi.product=p.id left join productpic pic on p.id=pic.productid where bi.user=? group by oi.id,p.name,o.orderno,bi.price,bi.num";
-		List<Record> records = Db.find(sql,getCurrentUserId());
-		Pager pager = new Pager(records.size(), records);
-		this.renderJson(records);
 
-		Pager pager = new Pager();
-		if (this.getPara("pageNumber") != null) {
-			int pagenumber = Integer.parseInt(this.getPara("pageNumber"));
-			int pagesize = Integer.parseInt(this.getPara("pageSize"));
-			Page<Record> page = Db.paginate(pagenumber, pagesize, "select * ", "from " + getModalName() + "");
-			pager = new Pager(page.getTotalRow(), page.getList());
-			List<Attribute> attributes = AttributeFinder.getInstance().getAllAttributeList(getCategory());
-			String searchword = this.getPara("searchword");
-			Iterator<Record> iter = pager.getRows().iterator();
-			this.renderJson(pager);
-		} else {
-			List<Record> records = Db.find("select * from " + getModalName());
-			pager = new Pager(records.size(), records);
-			List<Attribute> attributes = AttributeFinder.getInstance().getAllAttributeList(getCategory());
-			for (Record record : pager.getRows()) {
-				for (Attribute attribute : attributes) {
-					Attributevalue av = Attributevalue.dao.findFirst(
-							"select * from attributevalue where attributeid=? and objectid=? and category=?",
-							attribute.getAttributeid(), record.getInt("id"), getCategory());
-					if (av == null)
-						continue;
-					record.set("attribute-" + getCategory() + av.getAttributeid(), av.getValue());
-				}
-				preRenderJsonForList(record);
-			}
-			this.renderJson(records);
-		}
-	
-	*/}
+	public void list() {
+		String sql = "select oi.id id,p.name name,o.orderno orderno,bi.price price,bi.num num,GROUP_CONCAT(pic.fielname) filename,oi.date date";
+		String sqlExcept = " from orderitem oi left join bookitem bi on oi.bookitem=bi.id left join `order` o on o.id=oi.order left join product p on bi.product=p.id left join productpic pic on p.id=pic.productid where bi.user=? group by oi.id,p.name,o.orderno,bi.price,bi.num,oi.date order by oi.id desc";
+		int pagenumber = Integer.parseInt(this.getPara("pageNumber"));
+		int pagesize = Integer.parseInt(this.getPara("pageSize"));
+		Page<Record> page = Db.paginate(pagenumber, pagesize, sql, sqlExcept, getCurrentUserId());
+		Pager pager = new Pager(page.getTotalRow(), page.getList());
+		this.renderJson(pager);
+
+	}
 
 	@Override
 	public String getModalName() {
