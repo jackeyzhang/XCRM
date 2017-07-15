0e45ca4874855ce80e8f22e321bc60811d3100e1
jzhang12 <jzhang12@WL00070328.active.local>
2017-07-15 21:10:54 +0800

    fix report templates

--- XCRM/src/com/xcrm/order/OrderViewController.java
+++ XCRM/src/com/xcrm/order/OrderViewController.java
@@ -1,6 +1,9 @@
 package com.xcrm.order;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Page;
@@ -41,29 +44,12 @@ public class OrderViewController extends AbstractController {
 
   public void loadingPayment() {
     String orderno = this.getPara( "orderno" );
-    String sql = "select CONCAT(cust.name, '-' ,cust.company) customer,pay.comments,round(pay.paid,2) paid,pay.paymenttime,pay.paymenttype";
-    String sqlExcept = " from payment pay " + "left join customer cust on cust.id=pay.customerid " + "where pay.orderno = " + orderno + " order by pay.paymenttime desc";
-    Page<Record> page = Db.paginate( 1, 100, sql, sqlExcept );
-    Pager pager = new Pager( page.getTotalRow(), page.getList() );
-
-    this.renderJson( pager );
+    this.renderJson( getPayment( orderno ) );
   }
 
   public void list() {
     String orderno = this.getSessionAttr( "orderno" );
-    //price是原价  deal price是成交价  
-    String imgpath = getPrdImgBaseUrl() ;
-    String sql = "select concat('"+imgpath+"',CAST(p.id AS CHAR),'/',(select ppic.fielname from productpic ppic where ppic.productid=p.id limit 1)) filename," 
-        +" o.orderno,p.name name,bi.prdattrs attr,round(bi.price,2) price,round(o.price,2) dealprice,round(bi.price*bi.num*bi.discount/100*o.totaldiscount/100,2) itemamount,"
-        + "round(o.paid,2) paid,bi.num num,bi.discount discount,oi.date date,contract.name contractname,contract.id contractid,bi.comments comments,bi.additionfee afee";
-    String sqlExcept = " from orderitem oi " 
-        + "left join bookitem bi on oi.bookitem=bi.id " 
-        + "left join `order` o on o.id=oi.order " 
-        + "left join product p on bi.product=p.id "
-        + "left join contract contract on bi.contract=contract.id " + "where bi.status = 1 and o.orderno = " + orderno + " order by o.orderno desc";
-    Page<Record> page = Db.paginate( 1, 100, sql, sqlExcept );
-    Pager pager = new Pager( page.getTotalRow(), page.getList() );
-    this.renderJson( pager );
+    this.renderJson( getOrderItemViews( orderno ) );
   }
 
   @Override
@@ -89,5 +75,43 @@ public class OrderViewController extends AbstractController {
   @Override
   public int getCategory() {
     return Constant.CATEGORY_ORDERVIEW;
+  }
+  
+  public void wxvieworder() {
+    String orderno = this.getPara( "orderno" );
+    Pager pager1 = getOrderItemViews( orderno );
+    Pager pager2 = getPayment( orderno );
+    List<Record> records1 = pager1.getRows();
+    List<Record> records2 = pager2.getRows();
+    Map<String, Object> json = new HashMap<String, Object>();
+    json.put( "orderitems", records1 );
+    if ( !records2.isEmpty() ) {
+      json.put( "payments", records2 );
+    }
+    this.renderJson( json );
+  }
+  
+  private Pager getOrderItemViews( String orderno ){
+    //price是原价  deal price是成交价  
+    String imgpath = getPrdImgBaseUrl() ;
+    String sql = "select concat('"+imgpath+"',CAST(p.id AS CHAR),'/',(select ppic.fielname from productpic ppic where ppic.productid=p.id limit 1)) filename," 
+        +" o.orderno,p.name name,bi.prdattrs attr,round(bi.price,2) price,round(o.price,2) dealprice,round(bi.price*bi.num*bi.discount/100*o.totaldiscount/100,2) itemamount,"
+        + "round(o.paid,2) paid,bi.num num,bi.discount discount,oi.date date,contract.name contractname,contract.id contractid,bi.comments comments,bi.additionfee afee";
+    String sqlExcept = " from orderitem oi " 
+        + "left join bookitem bi on oi.bookitem=bi.id " 
+        + "left join `order` o on o.id=oi.order " 
+        + "left join product p on bi.product=p.id "
+        + "left join contract contract on bi.contract=contract.id " + "where bi.status = 1 and o.orderno = " + orderno + " order by o.orderno desc";
+    Page<Record> page = Db.paginate( 1, 100, sql, sqlExcept );
+    Pager pager = new Pager( page.getTotalRow(), page.getList() );
+    return pager;
+  }
+  
+  private Pager getPayment( String orderno ){
+    String sql = "select CONCAT(cust.name, '-' ,cust.company) customer,pay.comments,round(pay.paid,2) paid,pay.paymenttime,pay.paymenttype";
+    String sqlExcept = " from payment pay " + "left join customer cust on cust.id=pay.customerid " + "where pay.orderno = " + orderno + " order by pay.paymenttime desc";
+    Page<Record> page = Db.paginate( 1, 100, sql, sqlExcept );
+    Pager pager = new Pager( page.getTotalRow(), page.getList() );
+    return pager;
   }
 }
