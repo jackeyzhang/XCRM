764801d0ba34b1f6c310ef0a279714b85fc653ba
jzhang12 <jzhang12@WL00070328.active.local>
2017-08-02 09:12:12 +0800

    增加历史支付情况页面

--- XCRM/WebRoot/js/spayment.js
+++ XCRM/WebRoot/js/spayment.js
@@ -1,9 +1,24 @@
 $(function() {
 	var r = window.location.search;
 	var orderno = r.substring(r.indexOf("=")+1,r.length);
+	var customerid = $('#customerid').html();
 	init = {
 			$paymenttable : $('#paymenttable').bootstrapTable({
 				url : "/orderview/loadingPayment?orderno=" + orderno,
+				queryParamsType: "limit",
+				pageNumber: 1,
+				pageList: [10, 25, 50, 100],
+				clickToSelect:true,
+				exportDataType: "basic",
+				sidePagination: "server",//表格分页的位置  
+				onLoadSuccess:function(data){
+					if(xcpage.tableInitHandler)
+						xcpage.tableInitHandler(data);
+				}
+			}),
+	
+			$allpaymenttable : $('#allpaymenttable').bootstrapTable({
+				url : "/orderview/loadingAllPayments?customerid=" + customerid,
 				queryParamsType: "limit",
 				pageNumber: 1,
 				pageList: [10, 25, 50, 100],
@@ -36,5 +51,11 @@ $(function() {
 			}
 		});
 	});	
+	
+	$('#showallpayment').click(function () {
+		var checked = $('#showallpayment').is(':checked');
+		var customerid = $('#customerid').html();
+		$('#allpaymenttable').bootstrapTable('refresh',{url:"/orderview/loadingAllPayments?customerid=" + customerid + "&showall=" + checked });
+	});
 	
 });
--- XCRM/src/com/xcrm/order/OrderViewController.java
+++ XCRM/src/com/xcrm/order/OrderViewController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Page;
@@ -14,6 +15,7 @@ import com.xcrm.common.model.Order;
 import com.xcrm.common.util.Constant;
 import com.xcrm.common.util.RecordUtil;
 import com.xcrm.common.util.StrUtil;
+import com.xcrm.common.util.Tuple;
 
 
 public class OrderViewController extends AbstractController {
@@ -26,9 +28,12 @@ public class OrderViewController extends AbstractController {
     Record record = Db.findFirst( "select o.comments,o.paymentcomments,o.price,o.totaldiscount from `order` o where o.orderno=" + orderno );
     BigDecimal dealPrice = record.getBigDecimal( "price" );
     BigDecimal totaldiscount = record.getBigDecimal( "totaldiscount" );
-    Record paymentrecord = Db.findFirst( "select sum(paid) paid from payment where orderno=" + orderno + " group by orderno" );
+    Record paymentrecord = Db.findFirst( "select sum(paid) paid, customerid customer from payment where orderno=" + orderno + " group by orderno" );
     BigDecimal paid = paymentrecord == null ? new BigDecimal(0) : paymentrecord.getBigDecimal( "paid" );
+    Record customerrecord = Db.findFirst( "select bi.customer from bookitem bi left join orderitem oi on oi.bookitem = bi.id left join `order` ord on oi.order = ord.id where ord.orderno = " + orderno );
+    Integer customerid = customerrecord == null ? null : customerrecord.getInt( "customer" );
     this.setAttr( "ordercomments", record.getStr( "comments" ) );
+    this.setAttr( "customer", customerid );
     this.setAttr( "paymentcomments", record.getStr( "paymentcomments" ) );
     this.setAttr( "dealprice", StrUtil.formatPrice( dealPrice ) );
     this.setAttr( "paid", StrUtil.formatPrice( paid ) );
@@ -41,6 +46,12 @@ public class OrderViewController extends AbstractController {
       this.setAttr( "dueinfo2", "挂账:" + dues );
     else
       this.setAttr( "dueinfo3", "支付完成" );
+    Tuple<Number,Number> tuple = this.getTotalDueAndBillAndBalance( customerid );
+    this.setAttr( "own", StrUtil.formatPrice(  tuple.getFirst() ) );
+    this.setAttr( "bill", StrUtil.formatPrice(   tuple.getSecond()  ) );
+    Number own = tuple.getFirst() == null ? 0 :   tuple.getFirst();
+    Number bill = tuple.getSecond() == null ? 0 :  tuple.getSecond();
+    this.setAttr( "balance", StrUtil.formatPrice( own.floatValue() + bill.floatValue()  ) );
     super.index();
   }
 
@@ -52,6 +63,17 @@ public class OrderViewController extends AbstractController {
   public void list() {
     String orderno = this.getSessionAttr( "orderno" );
     this.renderJson( getOrderItemViews( orderno ) );
+  }
+  
+  public void loadingAllPayments(){
+    String customerid = this.getPara( "customerid" );
+    String showall = this.getPara("showall");
+    Boolean showAll = Boolean.valueOf( showall );
+    if( customerid == null || customerid.isEmpty() ) {
+      this.renderNull();
+    }else{
+      this.renderJson( getAllPayments( customerid, showAll ) );
+    }
   }
 
   @Override
@@ -116,5 +138,70 @@ public class OrderViewController extends AbstractController {
     Page<Record> page = Db.paginate( 1, 100, sql, sqlExcept );
     Pager pager = new Pager( page.getTotalRow(), page.getList() );
     return pager;
+  }
+  
+  private Pager getAllPayments( String customerid, Boolean showAll ){
+    String sql = 
+        "select  ord.orderno, "
+        + "oo.customer,"
+        + "oo.saler,"
+        + "ord.date orderdate, "
+        + "round(ord.totalprice,2) price, "
+        + "round(ord.price,2) dealprice,"
+        + "round(ifnull(sum(pay.paid),0),2) paid, "
+        + "round((ord.price- ifnull(sum(pay.paid),0)),2) own ";
+    String sqlExcept = 
+        " from `order` ord "
+        + "left join payment pay on ord.orderno = pay.orderno "
+        + "join ( select distinct(oi.order) oid,concat(cust.name, '-',cust.company ) customer, u.username saler "
+        + "from orderitem oi "
+        + "left join bookitem bi on oi.bookitem = bi.id "
+        + "left join customer cust on cust.id= bi.customer "
+        + "left join user u on u.id = bi.user "
+        + "where bi.customer= "+ customerid +") oo on oo.oid = ord.id "
+        + "where ord.status !=" + Order.STATUS_CANCELLED + " "
+        + "group by ord.orderno";
+    Page<Record> page = Db.paginate( 1, 100, sql, sqlExcept );
+    List<Record> records = page.getList().stream().filter( p -> {
+      if(showAll) return true;
+      return p.getBigDecimal( "own" ).floatValue() != 0;
+    } ).collect( Collectors.toList() );
+    Pager pager = new Pager( page.getTotalRow(), records );
+    return pager;
+  }
+  
+  private Tuple<Number,Number> getTotalDueAndBillAndBalance( int customerid ){
+  
+    String sql = 
+        "select ifnull(sum(ooo.own),0) own from ( select "
+        + "round((ord.price- ifnull(sum(pay.paid),0)),2) own "
+        + " from `order` ord "
+        + "left join payment pay on ord.orderno = pay.orderno "
+        + "join ( select distinct(oi.order) oid,concat(cust.name, '-',cust.company ) customer "
+        + "from orderitem oi "
+        + "left join bookitem bi on oi.bookitem = bi.id "
+        + "left join customer cust on cust.id= bi.customer "
+        + "where bi.customer= "+ customerid +") oo on oo.oid = ord.id "
+        + "where ord.status !=" + Order.STATUS_CANCELLED + " "
+        + "group by ord.orderno "
+        + ") ooo where ooo.own > 0 ";
+    Record record = Db.findFirst( sql );
+    
+    String sql1 = 
+            "select ifnull(sum(ooo.own),0) own from ( select "
+            + "round((ord.price- ifnull(sum(pay.paid),0)),2) own "
+            + " from `order` ord "
+            + "left join payment pay on ord.orderno = pay.orderno "
+            + "join ( select distinct(oi.order) oid,concat(cust.name, '-',cust.company ) customer "
+            + "from orderitem oi "
+            + "left join bookitem bi on oi.bookitem = bi.id "
+            + "left join customer cust on cust.id= bi.customer "
+            + "where bi.customer= "+ customerid +") oo on oo.oid = ord.id "
+            + "where ord.status !=" + Order.STATUS_CANCELLED + " "
+            + "group by ord.orderno "
+            + ") ooo where ooo.own < 0 ";
+    Record record1 = Db.findFirst( sql1 );
+    Tuple<Number,Number> number = new Tuple<Number,Number>(  record.getBigDecimal( "own" )  ,record1.getBigDecimal( "own" ) );
+    return number;
   }
 }
